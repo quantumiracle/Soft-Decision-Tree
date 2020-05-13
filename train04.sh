45e04948d77ac920f499d98db3653fc82655eba5
zihan <zhding@mail.ustc.edu.cn>
2020-05-13 17:42:19 -0400

    up

--- train04.sh
+++ train04.sh
@@ -5,7 +5,7 @@ echo $CUDA_VISIBLE_DEVICES
 
 # python3 sdt_train.py  --depth=3  &
 # python3 sdf_train.py --num_trees=3 --depth=3  & 
-python3 sdf_module_train.py --num_trees=9 --depth=5
+python3 sdf_module_train.py --num_trees=3 --depth=5
 
 min=3
 max=9
