34ad66d953887e6100d9a535b69ad59075e911b7
zihan <zhding@mail.ustc.edu.cn>
2020-05-13 09:37:43 -0400

    results

---
+++ train02.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# hostname
+echo $CUDA_VISIBLE_DEVICES
+
+# python3 sdt_train.py  --depth=3  &
+# python3 sdf_train.py --num_trees=3 --depth=3  & 
+# python3 sdf_module_train.py --num_trees=3 --depth=3  &
+
+min=3
+max=9
+inter=2
+
+
+for ((i=min; i <= max; i+=inter));
+do 
+    python3 sdf_train.py --num_trees=3 --depth="$i"
+done 
---
+++ train2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+hostname
+echo $CUDA_VISIBLE_DEVICES
+
+python3 sdt_train.py  --depth=3
+# python3 sdf_train.py --num_trees=3 --depth=3  & 
+# python3 sdf_module_train.py --num_trees=3 --depth=3  &
+ 
