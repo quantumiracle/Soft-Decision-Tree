b25750f456a41f450580e58585a4e2e796d59f92
quantumiracle <1402434478@qq.com>
2020-05-14 10:06:50 -0400

    clear results

--- train04.sh
+++ train04.sh
@@ -3,15 +3,6 @@
 hostname
 echo $CUDA_VISIBLE_DEVICES
 
-# python3 sdt_train.py  --depth=3  &
+python3 sdt_train.py  --depth=3  &
 # python3 sdf_train.py --num_trees=3 --depth=3  & 
-python3 sdf_module_train.py --num_trees=3 --depth=5
-
-min=3
-max=9
-inter=2
-
-#for ((i=min; i <= max; i+=inter));
-#do 
-#    python3 sdf_module_train.py --num_trees="$i" --depth=5 &
-#done 
+# python3 sdf_module_train.py --num_trees=3 --depth=5
