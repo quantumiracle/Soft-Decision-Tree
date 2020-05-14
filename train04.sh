e2c26f31e931716552860b0054d980e876a24f27
zihan <zhding@mail.ustc.edu.cn>
2020-05-14 10:19:14 -0400

    up

--- train04.sh
+++ train04.sh
@@ -3,6 +3,6 @@
 hostname
 echo $CUDA_VISIBLE_DEVICES
 
-python3 sdt_train.py  --depth=3  &
-# python3 sdf_train.py --num_trees=3 --depth=3  & 
+# python3 sdt_train.py  --depth=9 
+python3 sdf_train.py --num_trees=3 --depth=3 
 # python3 sdf_module_train.py --num_trees=3 --depth=5
