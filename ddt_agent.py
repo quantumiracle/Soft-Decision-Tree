6b07d3bc44b654ae885621f2ca7ac0872e13d4e4
quantumiracle <1402434478@qq.com>
2020-05-19 10:43:27 -0400

    update ddt agent

--- ddt_agent.py
+++ ddt_agent.py
@@ -10,18 +10,32 @@ from heuristic_tree_agent2 import run
 
 
 def DDT_action(s):
-    if s[5]>-0.04:
+    if s[5]>0.04:
         a=3
+    elif s[3]>0.05:
+        a=1
+    elif s[3]>0.26:
+        a=0
     elif s[3]>-0.35:
+        a=0
+    elif s[3]>-0.04:
         a=0
     elif s[5]>-0.22:
         a=0
     elif s[4]>-0.04:
         a=1
-    elif s[7]>0:
+    elif s[6]>0:
         a=2
     elif s[2]>0.32:
         a=2
+    elif s[6]>0:
+        a=3
+    elif s[5]>0.16:
+        a=2
+    elif s[3]>0.19:
+        a=3
+    elif s[3]>0.4:
+        a=0
     elif s[1]>-0.11:
         a=1
     elif s[4]>0.15:
