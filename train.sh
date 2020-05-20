496caab63f91001c90321112c9e7f826da89e436
zihan <zhding@mail.ustc.edu.cn>
2020-05-20 13:26:45 -0400

    up

--- train.sh
+++ train.sh
@@ -8,7 +8,7 @@ max=3
 inter=1
 for ((i=min; i <= max; i+=inter));
 do 
-    python3 sdt_train.py  --lamda=1e-1 --id="$i"
+    python3 sdt_train.py  --lamda=-1e-1 --id="$i"
     #python3 sdf_train.py --num_trees=3 --depth=9 --id="$i"
     #python3 sdf_module_train.py --num_trees=7 --depth=5 --id="$i"
 done
