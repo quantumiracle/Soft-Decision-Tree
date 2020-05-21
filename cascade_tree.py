15ea8129fff42a05e83c75efb2a04bec59dd5f02
quantumiracle <1402434478@qq.com>
2020-05-21 19:56:32 -0400

    add cascade tree

--- cascade_tree.py
+++ cascade_tree.py
@@ -4,33 +4,154 @@ import torch
 import torch.nn as nn
 from utils.dataset import Dataset
 import numpy as np
-from SDT import SDT
 
-class Cascade_SDT(nn.Module):
-    def __init__(self):
-        super(Cascade_SDT, self).__init__()
+class Cascade_DDT(nn.Module):
+    def __init__(self, args):
+        super(Cascade_DDT, self).__init__()
+        self.args = args
+        self.device = torch.device('cuda' if self.args['cuda'] else 'cpu')
 
-        features = self.feature_learning_module()
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
 
-        self.sparse_SDT(features)
+        self.feature_learning_init()
+        self.decision_init()
 
-    def feature_learning_module(self):
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.args['lr'], weight_decay=self.args['weight_decay'])
+        self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=self.args['exp_scheduler_gamma'])
 
-        pass
 
-    def sparse_SDT(self, features):
-        pass
+    def feature_learning_init(self):
+        self.num_fl_inner_nodes = 2**self.args['feature_learning_depth'] -1
+        self.num_fl_leaves = self.num_fl_inner_nodes + 1
+        self.fl_inner_nodes = nn.Linear(self.args['input_dim']+1, self.num_fl_inner_nodes, bias=False)
+        # coefficients of feature combinations
+        fl_leaf_weights = torch.randn(self.num_fl_leaves*self.args['num_intermediate_variables'], self.args['input_dim'])
+        self.fl_leaf_weights = nn.Parameter(fl_leaf_weights)
 
-    def forward(self,):
-        pass
+    def feature_learning_forward(self):
+        """ 
+        Forward the tree for feature learning.
+        Return the probabilities for reaching each leaf.
+        """
+        path_prob = self.sigmoid(self.fl_inner_nodes(self.aug_data))
 
-    
-    def load_model(self, path):
-        pass
+        path_prob = torch.unsqueeze(path_prob, dim=2)
+        path_prob = torch.cat((path_prob, 1-path_prob), dim=2)
+        _mu = self.aug_data.data.new(self.batch_size,1,1).fill_(1.)
 
-    def save_model(self, path):
-        pass
+        begin_idx = 0
+        end_idx = 1
+        for layer_idx in range(0, self.args['feature_learning_depth']):
+            _path_prob = path_prob[:, begin_idx:end_idx, :]
 
-    
+            _mu = _mu.view(self.batch_size, -1, 1).repeat(1, 1, 2)
+            _mu = _mu * _path_prob
+            begin_idx = end_idx  # index for each layer
+            end_idx = begin_idx + 2 ** (layer_idx+1)
+        mu = _mu.view(self.batch_size, self.num_fl_leaves)  
 
-if __name__ == '__main__':    
+        return mu        
+
+
+    def decision_init(self):
+        self.num_dc_inner_nodes = 2**self.args['decision_depth'] -1
+        self.num_dc_leaves = self.num_dc_inner_nodes + 1
+        self.dc_inner_nodes = nn.Linear(self.args['num_intermediate_variables']+1, self.num_dc_inner_nodes, bias=False)
+
+        dc_leaves = torch.randn(self.num_dc_leaves, self.args['output_dim'])
+        self.dc_leaves = nn.Parameter(dc_leaves)
+
+    def decision_forward(self):
+        """
+        Forward the differentiable decision tree
+
+        """
+        self.intermediate_features_construct()
+        aug_features = self._data_augment_(self.features)
+        path_prob = self.sigmoid(self.dc_inner_nodes(aug_features))
+        feature_batch_size = self.features.shape[0]
+
+        path_prob = torch.unsqueeze(path_prob, dim=2)
+        path_prob = torch.cat((path_prob, 1-path_prob), dim=2)
+        _mu = aug_features.data.new(feature_batch_size,1,1).fill_(1.)
+
+        begin_idx = 0
+        end_idx = 1
+        for layer_idx in range(0, self.args['decision_depth']):
+            _path_prob = path_prob[:, begin_idx:end_idx, :]
+
+            _mu = _mu.view(feature_batch_size, -1, 1).repeat(1, 1, 2)
+            _mu = _mu * _path_prob
+            begin_idx = end_idx  # index for each layer
+            end_idx = begin_idx + 2 ** (layer_idx+1)
+        mu = _mu.view(feature_batch_size, self.num_dc_leaves)  
+
+        return mu   
+
+    def intermediate_features_construct(self):
+        """
+        Construct the intermediate features for decision making, with learned feature combinations from feature learning module.
+        """
+        features = self.fl_leaf_weights.view(-1, self.args['input_dim']) @ self.data.transpose(0,1)   # data: (batch_size, feature_dim); return: (num_fl_leaves*num_intermediate_variables, batch)
+        self.features = features.contiguous().view(self.num_fl_leaves, self.args['num_intermediate_variables'], -1).permute(2,0,1).contiguous().view(-1, self.args['num_intermediate_variables'])  # return: (N, num_intermediate_variables) where N=batch_size*num_fl_leaves
+
+    def decision_leaves(self, p):
+        distribution_per_leaf = self.softmax(self.dc_leaves)
+        average_distribution = torch.mm(p, distribution_per_leaf)
+        return average_distribution
+
+    def forward(self, data, LogProb=True):
+        self.data = data
+        self.batch_size = data.size()[0]
+        self.aug_data = self._data_augment_(data)
+        fl_probs = self.feature_learning_forward()  # (batch_size, num_fl_leaves) 
+        dc_probs = self.decision_forward()
+        dc_probs = dc_probs.view(self.batch_size, self.num_fl_leaves, -1)   # (batch_size, num_fl_leaves, output_dim)
+
+        _mu = torch.bmm(fl_probs.unsqueeze(1), dc_probs).squeeze(1)  # (batch_size, output_dim)
+        output = self.decision_leaves(_mu)
+
+        # if self.args['greatest_path_probability']:
+        #     one_hot_path_probability = torch.zeros(_mu.shape).to(self.device)
+        #     vs, ids = torch.max(_mu, 1)  # ids is the leaf index with maximal path probability
+        #     one_hot_path_probability.scatter_(1, ids.view(-1,1), 1.)
+ 
+        #     prediction = self.leaf_nodes(one_hot_path_probability)
+        #     self.max_leaf_idx = ids
+
+        # else:  # prediction value equals to the average distribution
+        #     prediction = output
+
+        if LogProb:
+            output = torch.log(output)
+            # prediction = torch.log(prediction)
+        prediction = output
+
+        return prediction, output, 0
+        
+
+    """ Add constant 1 onto the front of each instance, serving as the bias """
+    def _data_augment_(self, input):
+        batch_size = input.size()[0]
+        input = input.view(batch_size, -1)
+        bias = torch.ones(batch_size, 1).to(self.device)
+        input = torch.cat((bias, input), 1)
+        return input
+
+    def save_model(self, model_path, id=''):
+        torch.save(self.state_dict(), model_path+id)
+
+    def load_model(self, model_path, id=''):
+        self.load_state_dict(torch.load(model_path+id, map_location='cpu'))
+        self.eval()
+
+
+if __name__ == '__main__':   
+    learner_args = {
+        'num_intermediate_variables': 3,
+        'feature_learning_depth': 2,
+        'decision_depth': 2,
+        'input_dim': 8,
+        'output_dim': 4,
+    }
---
+++ cascade_tree_train.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+from SDT import SDT
+from utils.dataset import Dataset
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+# from heuristic_evaluation import difference_metric
+from cascade_tree import Cascade_DDT
+
+def onehot_coding(target, device, output_dim):
+    target_onehot = torch.FloatTensor(target.size()[0], output_dim).to(device)
+    target_onehot.data.zero_()
+    target_onehot.scatter_(1, target.view(-1, 1), 1.)
+    return target_onehot
+
+use_cuda = True
+learner_args = {
+    'num_intermediate_variables': 3,
+    'feature_learning_depth': 3,
+    'decision_depth': 3,
+    'input_dim': 8,
+    'output_dim': 4,
+    'lr': 1e-3,
+    'weight_decay': 0.,  # 5e-4
+    'batch_size': 1280,
+    'exp_scheduler_gamma': 1.,
+    'cuda': use_cuda,
+    'epochs': 40,
+    'log_interval': 100,
+
+}
+learner_args['model_path'] = './model/trees/cascade_'+str(learner_args['feature_learning_depth'])
+
+
+device = torch.device('cuda' if use_cuda else 'cpu')
+
+def train_tree(tree):
+    writer = SummaryWriter(log_dir='runs/'+'cascade_'+str(learner_args['feature_learning_depth']))
+    # criterion = nn.CrossEntropyLoss()  # torch CrossEntropyLoss = LogSoftmax + NLLLoss
+    criterion = nn.NLLLoss()  # since we already have log probability, simply using Negative Log-likelihood loss can provide cross-entropy loss
+        
+    # Load data
+    data_dir = './data/discrete_'
+    data_path = data_dir+'state.npy'
+    label_path = data_dir+'action.npy'
+    train_loader = torch.utils.data.DataLoader(Dataset(data_path, label_path, partition='train'),
+                                    batch_size=learner_args['batch_size'],
+                                    shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(Dataset(data_path, label_path, partition='test'),
+                                    batch_size=learner_args['batch_size'],
+                                    shuffle=True)
+
+
+    # # test
+    # data_iter = iter(train_loader)
+    # (data, target)  = next(data_iter)
+    # print(data.shape)
+    # print(tree.forward(data.to(device)).shape)
+
+    # Utility variables
+    best_testing_acc = 0.
+    testing_acc_list = []
+    
+    for epoch in range(1, learner_args['epochs']+1):
+        epoch_training_loss_list = []
+        epoch_weight_difference_list = []
+        
+        # Training stage
+        tree.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            target_onehot = onehot_coding(target, device, learner_args['output_dim'])
+            prediction, output, penalty = tree.forward(data)
+
+            difference=0
+            # difference = difference_metric(weights)
+            # epoch_weight_difference_list.append(difference)
+            
+            tree.optimizer.zero_grad()
+            loss = criterion(output, target.view(-1))
+            loss += penalty
+            loss.backward()
+            tree.optimizer.step()
+            
+            # Print intermediate training status
+            if batch_idx % learner_args['log_interval'] == 0:
+                with torch.no_grad():
+                    pred = prediction.data.max(1)[1]
+                    correct = pred.eq(target.view(-1).data).sum()
+                    loss = criterion(output, target.view(-1))
+                    epoch_training_loss_list.append(loss.detach().cpu().data.numpy())
+                    print('Epoch: {:02d} | Batch: {:03d} | CrossEntropy-loss: {:.5f} | Correct: {}/{} | Difference: {}'.format(
+                            epoch, batch_idx, loss.data, correct, output.size()[0], difference))
+
+                    tree.save_model(model_path = learner_args['model_path'])
+        writer.add_scalar('Training Loss', np.mean(epoch_training_loss_list), epoch)
+        # writer.add_scalar('Training Weight Difference', np.mean(epoch_weight_difference_list), epoch)
+
+        # Testing stage
+        tree.eval()
+        correct = 0.
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+            batch_size = data.size()[0]
+            prediction, _, _ = tree.forward(data)
+            pred = prediction.data.max(1)[1]
+            correct += pred.eq(target.view(-1).data).sum()
+        accuracy = 100. * float(correct) / len(test_loader.dataset)
+        if accuracy > best_testing_acc:
+            best_testing_acc = accuracy
+        testing_acc_list.append(accuracy)
+        writer.add_scalar('Testing Accuracy', accuracy, epoch)
+        print('\nEpoch: {:02d} | Testing Accuracy: {}/{} ({:.3f}%) | Historical Best: {:.3f}% \n'.format(epoch, correct, len(test_loader.dataset), accuracy, best_testing_acc))
+
+
+if __name__ == '__main__':    
+    tree = Cascade_DDT(learner_args).to(device)
+    train_tree(tree)
