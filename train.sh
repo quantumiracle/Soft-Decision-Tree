433d82735abe315c41d2d84b1d3e9afb8598f276
quantumiracle <1402434478@qq.com>
2020-05-22 15:31:12 -0400

    set train

--- train.sh
+++ train.sh
@@ -6,4 +6,12 @@
 hostname
 echo $CUDA_VISIBLE_DEVICES
 
-python3 sdt_train.py
+min=1
+max=3
+inter=1
+for ((i=min; i <= max; i+=inter));
+do 
+    python3 cascade_tree_train.py  --depth1=2 --depth2=2 --vars=3 --id="$i"
+    #python3 sdf_train.py --num_trees=3 --depth=9 --id="$i"
+    #python3 sdf_module_train.py --num_trees=7 --depth=5 --id="$i"
+done
