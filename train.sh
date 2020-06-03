42db5bad3fcddb855d53441258abfe677dd8de0d
zihan <zhding@mail.ustc.edu.cn>
2020-06-03 15:10:44 -0400

    up

--- train.sh
+++ train.sh
@@ -14,7 +14,8 @@ do
     # python3 cascade_tree_train.py  --depth1=3 --depth2=3 --vars=2 --id="$i"
     #python3 cascade_tree_train_cartpole.py --depth1=2 --depth2=1 --vars=2 --id="$i"
     #python3 sdt_train.py --depth=3 --id="$i"
-    python3 sdt_train_cartpole.py --depth=2 --id="$i"
+    python3 sdt_train_ppo.py --depth=7 --id="$i"
+    #python3 sdt_train_cartpole.py --depth=2 --id="$i"
     #python3 sdf_train.py --num_trees=3 --depth=9 --id="$i"
     #python3 sdf_module_train.py --num_trees=7 --depth=5 --id="$i"
 done
