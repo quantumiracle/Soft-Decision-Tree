d838f81c61c9f8d5d2a64786beca706700368c8a
quantumiracle <1402434478@qq.com>
2020-06-09 18:29:46 -0400

    up obs wrapper

--- image_based_envs/env_wrapper.py
+++ image_based_envs/env_wrapper.py
@@ -40,33 +40,33 @@ class DiscreteActionWrapper(gym.Wrapper):
 
 
 class ObservationWrapper(gym.Wrapper):
-    def __init__(self, env, channel=None):
+    def __init__(self, env, selected_channel=None):
         """  
         Transform the observation for Atari games: downsample, select channel, truncate the edge pixels
 
-        channel: selected channel index if not None
-        
+        selected_channel: selected channel index if not None
+
         """
         super(ObservationWrapper, self).__init__(env)
         # switch order for observation space for using PyTorch model
         dim1, dim2, channel = env.observation_space.shape  
         self.downsample_rate=3
-        self.channel = channel
+        self.selected_channel = selected_channel
         self.dim1 = int((dim1-30)/self.downsample_rate)
         self.dim2 = int((dim2-10)/self.downsample_rate)
-        if isinstance(self.channel, int):
+        if isinstance(self.selected_channel, int):
             self.observation_space = spaces.Box(low=-np.inf,high=np.inf, shape=(1, self.dim1, self.dim2))
-        elif self.channel is None:
-            self.observation_space = spaces.Box(low=-np.inf,high=np.inf, shape=(1, self.dim1, self.dim2))
+        elif self.selected_channel is None:
+            self.observation_space = spaces.Box(low=-np.inf,high=np.inf, shape=(channel, self.dim1, self.dim2))
         else:
             raise NotImplementedError
         print(self.observation_space)
     
     def prepro(self, I):
         """Downsample 210x160x3 uint8 frame into 95x80x3."""
-        if isinstance(self.channel, int):
-            I=I[10:190, 10:, self.channel]
-        elif self.channel is None:
+        if isinstance(self.selected_channel, int):
+            I=I[10:190, 10:, self.selected_channel:self.selected_channel+1]  # preserve the third dimension
+        elif self.selected_channel is None:
             I=I[10:190, 10:]
         else:
             raise NotImplementedError
@@ -76,15 +76,13 @@ class ObservationWrapper(gym.Wrapper):
 
     def step(self, action):
         observation, reward, done, info = self.env.step(action)
-        # return np.moveaxis(self.prepro(observation), 2, 0), reward, done, info
-        return self.prepro(observation), reward, done, info
+        return np.moveaxis(self.prepro(observation), 2, 0), reward, done, info
+
 
 
     def reset(self, **kwargs):
         observation = self.env.reset(**kwargs)
-        # return np.moveaxis(self.prepro(observation), 2, 0)  # (H, W, C) -> (C, H, W)     
-        return self.prepro(observation)
-
+        return np.moveaxis(self.prepro(observation), 2, 0)  # (H, W, C) -> (C, H, W)     
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -95,16 +93,16 @@ if __name__ == '__main__':
     # env =gym.make(EnvName)
 
     EnvName = 'Freeway-v0'
-    env = ObservationWrapper(gym.make(EnvName))
+    env = ObservationWrapper(gym.make(EnvName), selected_channel=None)
 
     env.reset()
     for _ in range(10000):
         # env.render()
         a = env.action_space.sample()
         print(a)
-        s, r, d, _ = env.step(a) # take a random action
-        # plt.imshow(np.moveaxis(s, 0, 2))
-        plt.imshow(s)
+        s, r, d, _ = env.step(a) # take a random action\
+        plt.imshow(np.moveaxis(s, 0, 2))
+        # plt.imshow(np.moveaxis(s, 0, 2)[:, :, 0])  # when selected channel is int
         plt.show()
     env.close()
 
