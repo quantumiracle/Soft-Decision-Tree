2ae6ee78e21bc9a43fdaaa330a01827853ec3fa0
zihan <zhding@mail.ustc.edu.cn>
2020-07-31 23:54:09 -0400

    up

--- image_based_envs/minatar_test/train.sh
+++ image_based_envs/minatar_test/train.sh
@@ -6,8 +6,8 @@
 hostname
 echo $CUDA_VISIBLE_DEVICES
 
-#python3 ppo_gae_discrete_multi.py --train --id=0
+python3 ppo_gae_discrete_multi.py --train --id=0
 #python3 ppo_gae_discrete.py --train --id=0
 # python3 cdt_ppo_gae_discrete.py --train --depth1=3 --depth2=3 --id=0
-python3 cdt_ppo_gae_discrete_multi.py --train --depth1=3 --depth2=3 --id=0
+#python3 cdt_ppo_gae_discrete_multi.py --train --depth1=3 --depth2=3 --id=0
 
