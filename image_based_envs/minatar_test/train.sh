7bad9bf9a4e177f64e607a02d3eecd0ed3c60934
quantumiracle <1402434478@qq.com>
2020-08-23 10:52:13 +0800

    input norm

--- image_based_envs/minatar_test/train.sh
+++ image_based_envs/minatar_test/train.sh
@@ -9,5 +9,5 @@ echo $CUDA_VISIBLE_DEVICES
 #python3 ppo_gae_discrete_multi.py --train --id=0
 #python3 ppo_gae_discrete.py --train --id=0
 # python3 cdt_ppo_gae_discrete.py --train --depth1=3 --depth2=3 --id=0
-python3 cdt_ppo_gae_discrete_multi.py --train --depth1=6 --depth2=6 --id=0
+python3 cdt_ppo_gae_discrete_multi.py --train --depth1=4 --depth2=4 --id=0
 
